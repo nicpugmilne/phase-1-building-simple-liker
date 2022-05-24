98fe12ff3503e7d2d311265873994afbd8fb2e55
Nicola Milne <nicolalouisemilne@gmail.com>
2022-05-24 10:38:23 -0600

    commit tree 9ed75e8374ba1e1204a71af1849b4b917de9b178

--- main.js
+++ main.js
@@ -3,9 +3,29 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll('.like-glyph');
 
+for (const heart of hearts){
+  heart.addEventListener('click', handleLikes);
+}
 
-
+function handleLikes(e){
+  const clickedHeart= e.target;
+  mimicServerCall("")
+  .then(() => {
+    if(clickedHeart.textContent === EMPTY_HEART){
+      clickedHeart.textContent = FULL_HEART;
+      clickedHeart.classList.add("activated-heart");
+    } else if (clickedHeart.textContent === FULL_HEART){
+      clickedHeart.textContent = EMPTY_HEART;
+      clickedHeart.classList.remove("activated-heart");
+    }
+  })
+  .catch(() => {
+    document.querySelector('#modal').classList.remove("hidden");
+    setTimeout(() => document.querySelector('#modal').classList.add("hidden"), 3000);
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
